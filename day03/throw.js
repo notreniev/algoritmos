4ec64abe185fcf6735fa59e2ac4606f468013736
notreniev <everton.canez@gmail.com>
2019-10-07 19:03:11 -0300

    pequenas correções

--- day03/throw.js
+++ day03/throw.js
@@ -5,17 +5,16 @@
  * If 'a' is negative, throw an Error with the message "Negative Error"
  */
 function isPositive(a) {
-    if (1 > 0) {
+    if (a > 0) {
         return "YES"
-    } else if (1 === 0) {
+    } else if (a === 0) {
         throw new Error("Zero Error")
     } else {
         throw new Error("Negative Error")
     }
-
 }
 
-isPositive(3)
-isPositive(1)
-isPositive(2)
-isPositive(6)
+console.log(isPositive(3))
+console.log(isPositive(0))
+console.log(isPositive(2))
+console.log(isPositive(6))
