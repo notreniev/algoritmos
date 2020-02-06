818459c9432a89ce6ebd561b36fa9cd01d520a67
notreniev <everton.canez@gmail.com>
2020-02-06 08:22:23 -0300

    Day15 added

---
+++ day15/changeAndPrint.js
@@ -0,0 +1,18 @@
+/**
+ * What will happen? 
+ */
+
+const name = "Javascript James";
+var age = 31;
+let occupation = "Developer";
+
+function changeAndPrintValues() {
+    name = "Everton Silva"
+    age = 33;
+    occupation = "Coder";
+}
+
+changeAndPrintValues();
+console.log("Name", name);
+console.log("Age", age);
+console.log("Pccupation", occupation);
